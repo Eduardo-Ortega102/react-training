dc3985fc3ed980df3544c75212d8759c82bf6564
Eduardo Ortega <eduardo.ortega@edataconsulting.es>
2022-06-20 10:36:40 +0100

    Example of useImperativeHandle

--- 03-starting-project/src/components/Login/Login.js
+++ 03-starting-project/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useReducer, useContext } from "react";
+import { useState, useEffect, useReducer, useContext, useRef } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
@@ -30,6 +30,8 @@ const passwordReducer = (passwordState, action) => {
 };
 
 const Login = (props) => {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
   const context = useContext(AuthContext);
   const [formIsValid, setFormIsValid] = useState(false);
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
@@ -79,13 +81,20 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    context.onLogin(emailState.value, passwordState.value);
+    if (formIsValid) {
+      context.onLogin(emailState.value, passwordState.value);
+    } else if (!emailState.isValid) {
+      emailInputRef.current.focus();
+    } else {
+      passwordInputRef.current.focus();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+          ref={emailInputRef}
           id="email"
           type="email"
           label="E-Mail"
@@ -95,6 +104,7 @@ const Login = (props) => {
           onBlur={validateEmailHandler}
         />
         <Input
+          ref={passwordInputRef}
           id="password"
           type="password"
           label="Password"
@@ -104,7 +114,7 @@ const Login = (props) => {
           onBlur={validatePasswordHandler}
         />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
--- 03-starting-project/src/components/UI/Input/Input.js
+++ 03-starting-project/src/components/UI/Input/Input.js
@@ -1,6 +1,18 @@
+import { forwardRef, useRef, useImperativeHandle } from "react";
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const activate = () => {
+    inputRef.current.focus();
+  };
+
+  // The 'useImperativeHandle' should be the last approach to implement something
+  useImperativeHandle(ref, () => ({
+    focus: activate,
+  }));
+
   return (
     <div
       className={`${classes.control} ${
@@ -9,6 +21,7 @@ const Input = (props) => {
     >
       <label htmlFor={props.id}>{props.label}</label>
       <input
+        ref={inputRef}
         id={props.id}
         type={props.type}
         value={props.value}
@@ -17,6 +30,6 @@ const Input = (props) => {
       />
     </div>
   );
-};
+});
 
 export default Input;
