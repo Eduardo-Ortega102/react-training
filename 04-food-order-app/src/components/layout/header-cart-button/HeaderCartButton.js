c554f6725787ef68b5c76a213cbe4d18d732d3c8
Eduardo Ortega <eduardo.ortega@edataconsulting.es>
2022-06-20 16:30:43 +0100

    Add animation to button when amount of items changes

--- 04-food-order-app/src/components/layout/header-cart-button/HeaderCartButton.js
+++ 04-food-order-app/src/components/layout/header-cart-button/HeaderCartButton.js
@@ -1,17 +1,30 @@
 import CartIcon from "../../cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../../store/cart-context";
 
 const HeaderCartButton = (props) => {
   const context = useContext(CartContext);
+  const [shouldAnimate, setShouldAnimate] = useState(false);
   const numberOfCartItems = context.items.reduce(
     (count, item) => count + item.amount,
     0
   );
+  const buttonClasses = `${classes.button} ${
+    shouldAnimate ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (numberOfCartItems === 0) {
+      return;
+    }
+    setShouldAnimate(true);
+    const identifier = setTimeout(() => setShouldAnimate(false), 300);
+    return () => clearTimeout(identifier);
+  }, [numberOfCartItems]);
 
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
