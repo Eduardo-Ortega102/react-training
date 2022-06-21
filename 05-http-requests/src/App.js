cdbff79c7c75da134be8b1db83643446806bed37
Eduardo Ortega <eduardo.ortega@edataconsulting.es>
2022-06-21 15:17:50 +0100

    Send request on component load

--- 05-http-requests/src/App.js
+++ 05-http-requests/src/App.js
@@ -1,31 +1,32 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
+/*const fetchMoviesHandler = () => {
+                      fetch("https://swapi.dev/api/films/")
+                        .then((response) => response.json())
+                        .then((data) =>
+                          setMovies(
+                            data.results.map((movieData) => ({
+                              id: movieData.episode_id,
+                              title: movieData.title,
+                              openingText: movieData.opening_crawl,
+                              releaseDate: movieData.release_date,
+                            }))
+                          )
+                        );
+                    };*/
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  /*const fetchMoviesHandler = () => {
-                fetch("https://swapi.dev/api/films/")
-                  .then((response) => response.json())
-                  .then((data) =>
-                    setMovies(
-                      data.results.map((movieData) => ({
-                        id: movieData.episode_id,
-                        title: movieData.title,
-                        openingText: movieData.opening_crawl,
-                        releaseDate: movieData.release_date,
-                      }))
-                    )
-                  );
-              };*/
-  const fetchMoviesHandler = async () => {
+
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films45/");
+      const response = await fetch("https://swapi.dev/api/films/");
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
@@ -42,7 +43,12 @@ function App() {
       setError($error.message);
     }
     setIsLoading(false);
-  };
+  }, []);
+
+  useEffect(() => {
+    // with this, the request is sent always when the component is loaded
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   return (
     <React.Fragment>
