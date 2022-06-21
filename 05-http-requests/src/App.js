3322dca9ca94bca890d916acc65291fd94e93a38
Eduardo Ortega <eduardo.ortega@edataconsulting.es>
2022-06-21 14:45:12 +0100

    Example with fetch and async/await

--- 05-http-requests/src/App.js
+++ 05-http-requests/src/App.js
@@ -5,19 +5,31 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const fetchMoviesHandler = () => {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => response.json())
-      .then((data) =>
-        setMovies(
-          data.results.map((movieData) => ({
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          }))
-        )
-      );
+  /*const fetchMoviesHandler = () => {
+      fetch("https://swapi.dev/api/films/")
+        .then((response) => response.json())
+        .then((data) =>
+          setMovies(
+            data.results.map((movieData) => ({
+              id: movieData.episode_id,
+              title: movieData.title,
+              openingText: movieData.opening_crawl,
+              releaseDate: movieData.release_date,
+            }))
+          )
+        );
+    };*/
+  const fetchMoviesHandler = async () => {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+    setMovies(
+      data.results.map((movieData) => ({
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      }))
+    );
   };
 
   return (
